bdf37dd89bee607ec93e8327aa2a065ac76e2f6b
Bmjlee0126 <minjunlee28@cornerstonecollegiate.org>
2024-11-28 08:26:12 +0000

    Thu, Nov 28, 2024, 5:26 PM +09:00

--- Output.c
+++ Output.c
@@ -34,6 +34,40 @@ int main() {
     return 0;
 }
 
+#include <stdio.h>
+
+int main() {
+    int n = 10; // Number of rows for the top half of the diamond
+    int i, j;
+
+    // Print the top half of the diamond
+    for (i = 1; i <= n; i++) {
+        // Print spaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Print the bottom half of the diamond
+    for (i = n - 1; i >= 1; i--) {
+        // Print spaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
 
 
